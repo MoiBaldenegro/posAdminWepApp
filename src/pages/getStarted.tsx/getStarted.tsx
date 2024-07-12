f5090dd88475bbfb03e906d2ff17c593495aeb41
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-07-12 15:26:38 -0600

    build: :fire: Apt landing
    apt landing
    
    apt landing

---
+++ src/pages/getStarted.tsx/getStarted.tsx
@@ -0,0 +1,9 @@
+import styles from './getstarted.module.css';
+
+export default function GetStarted() {
+  return (
+    <main className={styles.container}>
+      <h1>Get started</h1>
+    </main>
+  );
+}
